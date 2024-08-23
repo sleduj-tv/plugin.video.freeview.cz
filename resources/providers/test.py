cd7d50a91d33fae13c360d774ab4dc7cb54257dc
TV CZ / SK <106910269+sleduj-tv@users.noreply.github.com>
2024-08-23 22:22:01 +0200

    Update test.py

--- resources/providers/test.py
+++ resources/providers/test.py
@@ -13,8 +13,8 @@ except ImportError:
 
 
 CHANNELS = {
-    'jojsvet':{'mpd':'https://dash2.antik.sk/stream/hisi_joj_svet/playlist_cbcs.mpd','hls':'http://88.212.15.47/live/test_joj_svet/playlist.m3u8'},
-    'pcsk':{'mpd':'https://dash2.antik.sk/stream/nvidia_prima_cool/playlist_cbcs.mpd','hls':'http://88.212.15.47/live/test_prima_cool_hd_hevc_50/playlist.m3u8'}
+    'jojsvet':{'hls':'http://88.212.15.47/live/test_joj_svet/playlist.m3u8'},
+    'pcsk':{'hls':'http://88.212.15.47/live/test_prima_cool_hd_hevc_50/playlist.m3u8'}
 }
 
 HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
@@ -23,20 +23,3 @@ def play(_handle, _addon, params):
     channel = params['channel']
     if not channel in CHANNELS:
         raise #TODO
-
-    prefer_mpd = xbmcplugin.getSetting(_handle, 'ockompd') == 'true'
-
-    channel = CHANNELS[channel]
-
-    if prefer_mpd:
-        li = xbmcgui.ListItem(path=channel['mpd']+'|'+urlencode(HEADERS))
-        li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
-        li.setProperty('inputstream','inputstream.adaptive') #kodi 19
-        li.setProperty('inputstream.adaptive.manifest_type','mpd')
-        xbmcplugin.setResolvedUrl(_handle, True, li)
-    else:
-        li = xbmcgui.ListItem(path=channel['hls']+'|'+urlencode(HEADERS))
-        li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
-        li.setProperty('inputstream','inputstream.adaptive') #kodi 19
-        li.setProperty('inputstream.adaptive.manifest_type','hls')
-        xbmcplugin.setResolvedUrl(_handle, True, li)
