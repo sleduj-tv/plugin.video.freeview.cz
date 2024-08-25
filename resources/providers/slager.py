3071b15f07ec4a4e7311f4b235a07d4285ee7973
TV CZ / SK <106910269+sleduj-tv@users.noreply.github.com>
2024-08-25 11:53:37 +0200

    Update slager.py

--- resources/providers/slager.py
+++ resources/providers/slager.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Author: cache-sk
-# Created on: 10.10.2019
+# Created on: 19.12.2021
 # License: AGPL v.3 https://www.gnu.org/licenses/agpl-3.0.html
 
 import xbmcgui
@@ -13,16 +13,20 @@ except ImportError:
 
 
 CHANNELS = {
-    'slagr':{'hls':'https://stream-13.mazana.tv/slagroriginal.m3u8s'},
-    'slagr2':{'hls':'https://stream-23.mazana.tv/slagrmuzika.m3u8s'},
-    'slagrpremium':{'hls':'https://stream-17.mazana.tv/slagrpremium.m3u8s'},
+    'slagr':'https://stream-13.mazana.tv/slagroriginal.m3u8s',
+    'slagr2':'https://stream-23.mazana.tv/slagrmuzika.m3u8s',
+    'slagrpremium':'https://stream-17.mazana.tv/slagrpremium.m3u8s'
 }
 
-HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36','referer':'http://live.streaming.sk/'}
 
 def play(_handle, _addon, params):
     channel = params['channel']
     if not channel in CHANNELS:
         raise #TODO
 
-    channel = CHANNELS[channel]
+    li = xbmcgui.ListItem(path=CHANNELS[channel]+'|'+urlencode(HEADERS))
+    li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
+    li.setProperty('inputstream','inputstream.adaptive') #kodi 19
+    li.setProperty('inputstream.adaptive.manifest_type','hls')
+    xbmcplugin.setResolvedUrl(_handle, True, li)
