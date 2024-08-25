d5c8173f1b4f27cd5b57b9cdb3dc5ac7429b87c4
TV CZ / SK <106910269+sleduj-tv@users.noreply.github.com>
2024-08-25 11:51:18 +0200

    Update slager.py

--- resources/providers/slager.py
+++ resources/providers/slager.py
@@ -25,19 +25,4 @@ def play(_handle, _addon, params):
     if not channel in CHANNELS:
         raise #TODO
 
-    prefer_mpd = xbmcplugin.getSetting(_handle, 'ockompd') == 'true'
-
     channel = CHANNELS[channel]
-
-    if prefer_mpd:
-        li = xbmcgui.ListItem(path=channel['mpd']+'|'+urlencode(HEADERS))
-        li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
-        li.setProperty('inputstream','inputstream.adaptive') #kodi 19
-        li.setProperty('inputstream.adaptive.manifest_type','mpd')
-        xbmcplugin.setResolvedUrl(_handle, True, li)
-    else:
-        li = xbmcgui.ListItem(path=channel['hls']+'|'+urlencode(HEADERS))
-        li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
-        li.setProperty('inputstream','inputstream.adaptive') #kodi 19
-        li.setProperty('inputstream.adaptive.manifest_type','hls')
-        xbmcplugin.setResolvedUrl(_handle, True, li)
